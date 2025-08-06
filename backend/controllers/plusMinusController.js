870c2342d1e139a15e54fb0bb91b6983676e1dcd
Yash Thakkar <yashthakkar@Yashs-MacBook-Air.local>
2025-08-06 15:57:27 +0530

    Fix getDailyPlusMinus endpoint to use correct sell rate calculation

--- backend/controllers/plusMinusController.js
+++ backend/controllers/plusMinusController.js
@@ -18,7 +18,11 @@ const getDailyPlusMinus = async (req, res) => {
             SUM(pm.buy_total) / SUM(pm.buy_quantity * 1000) * 10
           ELSE 0 
         END as avg_buy_rate,
-        AVG(pm.avg_sell_rate) as avg_sell_rate
+        CASE 
+          WHEN SUM(pm.sell_quantity * 1000) > 0 THEN 
+            SUM(pm.sell_total) / SUM(pm.sell_quantity * 1000) * 10
+          ELSE 0 
+        END as avg_sell_rate
       FROM plus_minus pm
       LEFT JOIN items i ON pm.item_id = i.id
       WHERE 1=1
